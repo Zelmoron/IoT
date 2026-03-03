893a723775b65b911c7302006d90420feb906028
mumok4 <nosachenkoalbert@gmail.com>
2026-03-03 15:15:16 +0800

    added direction control

--- car_control/control.ino
+++ car_control/control.ino
@@ -4,23 +4,65 @@
 #define DIR_LEFT 7
 #define SPEED_LEFT 6
 
+#define LEFT_FORWARD LOW
+#define LEFT_BACKWARD HIGH
+
+#define RIGHT_FORWARD LOW
+#define RIGHT_BACKWARD HIGH
+
+void move(bool left_dir, int left_speed, bool right_dir, int right_speed) {
+  digitalWrite(DIR_LEFT, left_dir);
+  digitalWrite(DIR_RIGHT, right_dir);
+  analogWrite(SPEED_LEFT, left_speed);
+  analogWrite(SPEED_RIGHT, right_speed);
+}
+
+void forward(int speed) {
+  move(LEFT_FORWARD, speed, RIGHT_FORWARD, speed);
+}
+
+void backward(int speed) {
+  move(LEFT_BACKWARD, speed, RIGHT_BACKWARD, speed);
+}
+
+void turn_left(int steepness) {
+  move(LEFT_FORWARD, 0, RIGHT_FORWARD, steepness);
+}
+
+void turn_right(int steepness) {
+  move(LEFT_FORWARD, steepness, RIGHT_FORWARD, 0);
+}
+
+
+void rotate_left(int speed) {
+  move(LEFT_BACKWARD, speed, RIGHT_FORWARD, speed);
+
+}
+
+void rotate_right(int speed) {
+  move(LEFT_FORWARD, speed, RIGHT_BACKWARD, speed);
+}
+
 void setup() {
-  pinMode(DIR_LEFT, OUTPUT);
-  pinMode(SPEED_LEFT, OUTPUT);
   pinMode(DIR_RIGHT, OUTPUT);
   pinMode(SPEED_RIGHT, OUTPUT);
-  digitalWrite(DIR_LEFT, HIGH);
-  digitalWrite(DIR_RIGHT, LOW);
-  digitalWrite(SPEED_LEFT, HIGH);
-  digitalWrite(SPEED_RIGHT, LOW);
+  pinMode(DIR_LEFT, OUTPUT);
+  pinMode(SPEED_LEFT, OUTPUT);
+
+  forward(255);
   delay(2000);
-  digitalWrite(DIR_LEFT, LOW);
-  digitalWrite(DIR_RIGHT, LOW);
-  digitalWrite(SPEED_LEFT, LOW);
-  digitalWrite(SPEED_RIGHT, HIGH);
+  backward(255);
+  delay(2000);
+  turn_left(255);
+  delay(2000);
+  turn_right(255);
+  delay(2000);
+  rotate_left(255);
+  delay(2000);
+  rotate_right(255);
+  delay(2000);
+  forward(0);
 }
 
 void loop() {
-    
 }
-
