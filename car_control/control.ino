fe47d330c4a3c214fbd97fde4dc7f4f5dc8e9676
mumok4 <nosachenkoalbert@gmail.com>
2026-03-03 14:51:43 +0800

    tested speed

--- car_control/control.ino
+++ car_control/control.ino
@@ -1,20 +1,26 @@
-#define DIR_1 4
-#define SPEED_1 5
+#define DIR_RIGHT 4
+#define SPEED_RIGHT 5
 
-#define DIR_2 7
-#define SPEED_2 6
+#define DIR_LEFT 7
+#define SPEED_LEFT 6
 
 void setup() {
-  pinMode(DIR_1, OUTPUT);
-  pinMode(SPEED_1, OUTPUT);
-  pinMode(DIR_2, OUTPUT);
-  pinMode(SPEED_2, OUTPUT);
-
+  pinMode(DIR_LEFT, OUTPUT);
+  pinMode(SPEED_LEFT, OUTPUT);
+  pinMode(DIR_RIGHT, OUTPUT);
+  pinMode(SPEED_RIGHT, OUTPUT);
+  digitalWrite(DIR_LEFT, HIGH);
+  digitalWrite(DIR_RIGHT, LOW);
+  digitalWrite(SPEED_LEFT, HIGH);
+  digitalWrite(SPEED_RIGHT, LOW);
+  delay(2000);
+  digitalWrite(DIR_LEFT, LOW);
+  digitalWrite(DIR_RIGHT, LOW);
+  digitalWrite(SPEED_LEFT, LOW);
+  digitalWrite(SPEED_RIGHT, HIGH);
 }
 
 void loop() {
-  digitalWrite(DIR_1, LOW);
-  digitalWrite(DIR_2, HIGH);
-  digitalWrite(SPEED_1, LOW);
-  digitalWrite(SPEED_2, HIGH);
+    
 }
+
